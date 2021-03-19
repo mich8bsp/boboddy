e1abc574cb850340449b59312514fbce810d29cd
Michael Bespalov <michael.bespalov@genoox.com>
2021-03-19 17:11:31 +0200

    boboddy

---
+++ core/src/main/java/com/github/bbd/BoboddyMain.java
@@ -0,0 +1,7 @@
+package com.github.bbd;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class BoboddyMain extends ApplicationAdapter {
+}
