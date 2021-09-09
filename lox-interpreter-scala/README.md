6e9f919711e29db53c71bc9173265131bcae76ab
Michael Bespalov <mich8bsp@gmail.com>
2021-09-09 17:41:40 +0300

    don't allow defining variable with name which was already used to define a variable in same or outer scope

--- lox-interpreter-scala/README.md
+++
@@ -1,3 +0,0 @@
-# ScaLox
-
-Building a compiler as described in ["Crafting Interpreters"](https://craftinginterpreters.com/) book by Robert Nystrom (using Scala instead of Java).
--- lox-interpreter-scala/src/main/scala/com/github/boboddy/Stmt.scala
+++
@@ -1,8 +0,0 @@
-package com.github.boboddy
-
-sealed trait Stmt
-
-case class PrintStmt(expr: Expr) extends Stmt
-case class ExpressionStmt(expr: Expr) extends Stmt
-case class VarStmt(name: Token, initializer: Option[Expr]) extends Stmt
-case class BlockStmt(statements: Seq[Stmt]) extends Stmt
