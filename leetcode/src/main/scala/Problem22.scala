1e5efcb45b211a9d44d3bc348f414b07593b795f
Michael Bespalov <mich8bsp@gmail.com>
2021-11-04 00:45:54 +0200

    leetcoding

--- leetcode/src/main/scala/Problem22.scala
+++ leetcode/src/main/scala/Problem22.scala
@@ -5,30 +5,26 @@ object Problem22 {
   object Solution {
 
     private val cache: mutable.Map[Int, List[String]] = mutable.Map[Int, List[String]]()
+    private val sumCombinationsCache: mutable.Map[Int, List[List[Int]]] = mutable.Map[Int, List[List[Int]]]()
 
-    def generateNumberComponents(n: Int): List[List[Int]] = n match {
+    def getSumCombinations(n: Int): List[List[Int]] = n match {
       case 0 => List()
       case 1 => List(List(1))
       case _ =>
-        (1 until n).flatMap(i => generateNumberComponents(n-i).map(r => i::r)).toList ++ List(List(n))
+        (1 until n).flatMap(i => sumCombinationsCache.getOrElseUpdate(n-i, getSumCombinations(n-i)).map(r => i::r)).toList ++ List(List(n))
     }
 
     def generateParenthesis(n: Int): List[String] = n match {
-      case 1 => cache.getOrElseUpdate(1, List("()"))
+      case 0 => List("")
       case _ =>
         cache.getOrElseUpdate(n, {
-          val componentsOptions = generateNumberComponents(n)
+          val componentsOptions = getSumCombinations(n)
           componentsOptions.flatMap({
-            case List(x) if x == n => cache.getOrElseUpdate(n-1, generateParenthesis(n-1))
+            case List(x) if x == n => generateParenthesis(n-1)
               .map(x => s"($x)")
-            case components => components.map(i => cache.getOrElseUpdate(i, generateParenthesis(i)))
+            case components => components.map(i => generateParenthesis(i))
               .reduceLeft((prev, curr) => {
-                for{
-                  x: String <- prev
-                  y: String <- curr
-                }yield{
-                  s"$x$y"
-                }
+                for(x <- prev; y <- curr) yield s"$x$y"
               })
           }).distinct
         })
@@ -36,14 +32,6 @@ object Problem22 {
   }
 
   def main(args: Array[String]): Unit = {
-//    println(Solution.generateNumberComponents(1))
-//    println(Solution.generateNumberComponents(2))
-//    println(Solution.generateNumberComponents(3))
-//    println(Solution.generateNumberComponents(4))
-//    println(Solution.generateNumberComponents(5))
-//    println(Solution.generateNumberComponents(6))
-//    println(Solution.generateNumberComponents(7))
-//    println(Solution.generateNumberComponents(8))
     println(Solution.generateParenthesis(4))
   }
 }
